ef0043082da36d026ae8730d66ca6bb85b43d0f4
santiago alan <santiagoalan1@gmail.com>
2021-03-25 16:00:14 -0600

    Datos de contacto

--- process-bolsa.php
+++ process-bolsa.php
@@ -48,14 +48,14 @@ try {
     $mail->isSMTP();                                       //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = '';                     //SMTP username
-    $mail->Password   = '';                               //SMTP password
+    $mail->Username   = 'palmex.testing@gmail.com';                     //SMTP username
+    $mail->Password   = 'Pass.word1';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
     //Recipients
-    $mail->setFrom('', 'Mailer');
-    $mail->addAddress('', 'Joe User');     //Add a recipient
+    $mail->setFrom('palmex.testing@gmail.com', 'Mailer');
+    $mail->addAddress('palmex.testing@gmail.com', 'Joe User');     //Add a recipient
     
     if($tmp_file <> null)
         $mail->addAttachment($tmp_file, $Name .'-'.$Lastname .'-'.$Role.'.pdf');         //Add attachments
